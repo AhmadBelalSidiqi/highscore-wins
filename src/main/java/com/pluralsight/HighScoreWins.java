452e3b0e1b953419e6dcbbcd990d954257c1a153
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-04-14 13:36:24 -0400

    EXERCISE 3 complete.

--- src/main/java/com/pluralsight/HighScoreWins.java
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class HighScoreWins {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the score: ");
+        String userInput = scanner.nextLine();
+
+        String[] namesScoreSplit = userInput.split(Pattern.quote("|"));
+        String[] namesSplits = namesScoreSplit[0].split(":");
+        String home = namesSplits[0];
+        String visitor = namesSplits[1];
+
+        String[] scors = namesScoreSplit[1].split(":");
+        int homeScore = Integer.parseInt(scors[0]);
+        int visitorScore = Integer.parseInt(scors[1]);
+
+        if (homeScore > visitorScore){
+        System.out.println("Winner : " + home);}
+
+        if (homeScore < visitorScore){
+            System.out.println("Winner : " + visitor);}
+
+
+
+
+    }
 }
